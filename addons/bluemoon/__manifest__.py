21affc5ffd5b448b071f5136f64b4054a6da9030
pedro09pm <pedro09pm.principal@gmail.com>
2024-02-08 16:53:57 +0100

    Initial Commit;

--- addons/bluemoon/__manifest__.py
+++ addons/bluemoon/__manifest__.py
@@ -25,6 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'views/menu.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
--- addons/bluemoon/models/models.py
+++ addons/bluemoon/models/models.py
@@ -1,18 +1,53 @@
 # -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
+class Simulation(models.Model):
+    _name = 'bluemoon.simulation'
+    _description = 'Simulation'
 
-class bluemoon(models.Model): # Example model so module shows up <-- REMOVE!
-    _name = 'bluemoon.bluemoon'
-    _description = 'bluemoon.bluemoon'
+    name = fields.Char(string="Name", compute="_compute_name", store=True)
+    silverQuantity = fields.Float(string="Silver Quantity", required=True)
+    silverPrice = fields.Float(string="Silver Price", required=True)
+    user = fields.Many2one('bluemoon.user', string="User", required=True)
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
-        
-    @api.depends('value')
-    def _value_pc(self):
+    total = fields.Float(string="Total", compute="_compute_total", store=True)
+
+    @api.depends('silverQuantity', 'silverPrice')
+    def _compute_total(self):
         for record in self:
-            record.value2 = float(record.value) / 100
+            record.total = record.silverQuantity * record.silverPrice
+
+    @api.depends('user')
+    def _compute_name(self):
+        for record in self:
+            record.name = record.user.name + " Simulation - " + str(record.id)
+
+class User(models.Model):
+    _name = 'bluemoon.user'
+    _description = 'User'
+
+    name = fields.Char(string="Name", required=True)
+    email = fields.Char(string="Email", required=True)
+    phone = fields.Char(string="Phone", required=True)
+    address = fields.Char(string="Address", required=True)
+
+    simulations = fields.One2many('bluemoon.simulation', 'user', string="Simulations")
+
+class Bluemoon_Catalogue(models.Model):
+    _name = 'bluemoon.catalogue'
+    _description = 'Catalogue for Blue Moon'
+
+    name = fields.Char(string="Name", required=True, help="Introduce the name of material")
+    material_type = fields.Selection([
+        ('0', 'Steel'),
+        ('1', 'Silver'),
+        ('2', 'Other')
+    ], string="Material Type", required=True)
+    price = fields.Float(string="Price")
+    size = fields.Integer(string="Size")
+    silver_quality = fields.Selection([
+        ('800', '800 (80% purity)'),
+        ('900', '900 (90% purity)'),
+        ('925', '925 (92.5% purity)'),
+        ('999', '999 (100% purity)')
+    ], help="Select the purity level for silver")
